d96364dbfa19bdef0194ab578c51312805bda1e9
chris sullivan <107879600+chrisAsullivan@users.noreply.github.com>
2022-06-20 20:10:38 +0000

    Initial commit

---
+++ READme.md
@@ -0,0 +1,15 @@
+## input
+inputr is the way the computer gets info from the user
+
+note: python initally treats input as a string type 
+
+ex
+
+5 -> "5"
+
+today is monday -> "today is monday"
+### formula
+variablename = input("prompt")
+the computer asks the user 
+
+python stores the __input__ in the __variablename__
---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+userName = input("enter your name: ")
+print("hey " + userName)
+
+userGrade = input("what grade are u in: ")
+print("hey " + userName + ". " + userGrade + " is a lame grade")
+
+userYear = int(input("what year were u born: "))
+userAge = 2022 - userYear
+print(str(userAge) + " is so young")
+'''
+
+#create your own madlibs
+# have at least 5 inputs
+noun1 = input("please give me a noun: ")
+verb1 = input("please give me a verb: ")
+noun2 = input("please give me another noun: ")
+verb2 = input("please give me another verb: ")
+noun0verb = input("give me a verb thats also a noun: ")
+
+print("i had a gameboy in middle school and during lunch i played pokemon yellow on it") input()
